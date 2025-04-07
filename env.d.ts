de79c5d60e671c98f8e7b76bc3fa370948ec8d2a
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2025-04-06 21:41:25 -0300

    chore(config): variables de entorno, tsconfig y dependencias
    - Añade tipado para env variables en `env.d.ts`
    - Declara redirect URI, client ID y endpoints en `.env`
    - Agrega dependencias: expo-auth-session, secure-store, async-storage, web-browser
    - Ajusta platforms en `app.json`
    - Actualiza `App.tsx` para usar `theme` de contexto

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+        EXPO_PUBLIC_API_URL_WEB:string;
+        EXPO_PUBLIC_GOOGLE_CLIENT_ID: string;
+        EXPO_PUBLIC_AUTHORIZATION_ENDPOINT: string;
+        EXPO_PUBLIC_TOKEN_ENDPOINT: string
+    }
+}
