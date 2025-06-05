a57ae5bd320248fb4e512a3c603434e2f31b88bc
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2025-06-05 18:42:15 -0300

    refactor(manager-layout): integra header y footer personalizados en _layout.tsx
    - Reemplaza Stack por layout visual con ManagerHeader y ManagerFooter
    - Usa Slot para render dinámico del contenido
    - Aplica estilos básicos para estructura vertical

--- src/app/(manager)/_layout.tsx
+++ src/app/(manager)/_layout.tsx
@@ -1,6 +1,25 @@
 // src/app/(manager)/_layout.tsx
-import { Stack } from 'expo-router';
+import ManagerFooter from '@features/users/manager/components/layout/footer/ManagerFooter.web';
+import ManagerHeader from '@features/users/manager/components/layout/header/ManagerHeader';
+import { Slot} from 'expo-router';
+import { View, StyleSheet 
+ } from 'react-native';
 
-export default function AuthLayout() {
-  return <Stack />;
+export default function ManagerLayout() {
+  return (    <View style={styles.container}>
+      <ManagerHeader />
+      <View style={styles.content}>
+        <Slot />
+      </View>
+      <ManagerFooter/>
+    </View>);
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+  },
+});
--- src/features/restaurant/hooks/useGetRestaurantsByManager.ts
+++ src/features/restaurant/hooks/useGetRestaurantsByManager.ts
@@ -1,14 +1,16 @@
 // src/app/features/restaurant/hooks/useGetRestaurantsByManager.ts
 
-import { useEffect, useState } from 'react';
-import { getRestaurantsByManager } from '../services/restaurantBaseApiService';
-import { IRestaurant } from '@delatte/shared/interfaces';
-
 /**
  * Hook para obtener todos los restaurantes gestionados por un manager.
  *
  * @param managerId ID del manager
  */
+
+import { useEffect, useState } from 'react';
+import { getRestaurantsByManager } from '../services/restaurantBaseApiService';
+import { IRestaurant } from '@delatte/shared/interfaces';
+
+
 export const useGetRestaurantsByManager = (managerId: string) => {
   const [restaurants, setRestaurants] = useState<IRestaurant[]>([]);
   const [loading, setLoading] = useState(true);
